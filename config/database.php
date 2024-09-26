788097b67b8e9294ad1a5b320d2531437d0c887b
Wallace Stenio Da Silva <wallace.silva.go@mprj.mp.br>
2024-09-26 13:23:44 -0300

    aquivo .sqlite criado, conexão configurada e migration realizada com sucesso

--- config/database.php
+++ config/database.php
@@ -31,12 +31,12 @@ return [
 
     'connections' => [
 
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+       'sqlite' => [
+        'driver' => 'sqlite',
+        'url' => env('DATABASE_URL'),
+        'database' => env('DB_DATABASE', database_path('database.sqlite')),
+        'prefix' => '',
+        'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
             'journal_mode' => null,
             'synchronous' => null,
